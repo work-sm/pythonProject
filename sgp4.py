d524b86bde4ccc75bb9194baf1619b0750be5708
MaShaoXu <654299054@qq.com>
2020-10-28 22:05:13 +0800

    init

---
+++ sgp4.py
@@ -0,0 +1,5 @@
+from skyfield.api import load
+
+stations_url = 'http://celestrak.com/NORAD/elements/stations.txt'
+satellites = load.tle_file(stations_url)
+print('Loaded', len(satellites), 'satellites')
